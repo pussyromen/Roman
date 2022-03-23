c8f1314112f8331c6f9022530a52bd6782385e72
pussyromen <102221786+pussyromen@users.noreply.github.com>
2022-03-23 13:06:35 +0300

    Create crypto.js
    test

---
+++ crypto.js
@@ -0,0 +1,24 @@
+function Block(letter, rate, used, father, code) {
+this.letter = letter;
+this.rate = rate;
+this.used = used;
+this.father = father;
+this.code = code;
+function FindCode(block){
+if (tree[block.father].code != '*) {
+block.code = tree[block.father].code +
+*1':
+}
+else {
+if (block.letter == tree[minIndex].letter) {
+block.code = '0":
+7
+}
+else if (block.letter == tree[preminIndex].letter)
+block.code
++1°:
+else {
+FindCode(tree[block.father]);
+block.code = tree[block.father].code
++
+".::
